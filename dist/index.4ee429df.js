283d663f49066d8be88fa017e04bf7338c2d5342
habib <habibdev96@gmail.com>
2021-07-28 15:43:48 -0600

    selected dom elements & created calculate button

--- dist/index.4ee429df.js
+++ dist/index.4ee429df.js
@@ -382,6 +382,46 @@ function hmrAcceptRun(bundle/*: ParcelRequire */ , id/*: string */ ) {
 
 },{}],"SXDIM":[function(require,module,exports) {
 "use scrict";
+const billEl = document.querySelector("#bill");
+const peopleEl = document.querySelector("#people");
+const tipButtons = document.querySelectorAll(".option--btn");
+const customEl = document.querySelector("#custom");
+const totalTipEl = document.querySelector("#amount-tip");
+const totalPersonEl = document.querySelector("#amount-person");
+const calculateBtn = document.querySelector("#calculate");
+const resetBtn = document.querySelector("#reset");
+const billAmount = billEl.value.trim();
+const peopleAmount = peopleEl.value.trim();
+let totalPerPerson = billAmount / peopleAmount;
+const calculateTotalPerPerson = ()=>{
+    totalPerPerson = Math.round(totalPerPerson * 100) / 100;
+    totalPerPerson = totalPerPerson.toFixed(2);
+    if (billAmount && peopleAmount) totalPersonEl.innerText = `$${totalPerPerson}`;
+    else totalPersonEl.innerText = `$0.00`;
+};
+const resetInputs = ()=>{
+    billEl.value = null;
+    peopleEl.value = null;
+    customEl.value = null;
+    totalTipEl.innerText = "$0.00";
+    totalPersonEl.innerText = "$0.00";
+};
+tipButtons.forEach((btn)=>{
+    btn.addEventListener("click", ()=>{
+        const value = btn.value;
+        console.log(value);
+    });
+});
+customEl.addEventListener("input", ()=>{
+    const custom = customEl.value;
+    console.log(custom);
+});
+calculateBtn.addEventListener("click", ()=>{
+    calculateTotalPerPerson();
+});
+resetBtn.addEventListener("click", ()=>{
+    resetInputs();
+});
 
 },{}]},["1XQsC","SXDIM"], "SXDIM", "parcelRequirea361")
 
