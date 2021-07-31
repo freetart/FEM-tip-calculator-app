6c61748cba895f652130376e2ab682998c6aa088
habib <habibdev96@gmail.com>
2021-07-30 21:12:33 -0600

    added calculator functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,53 +1,109 @@
 "use scrict";
 
-const billEl = document.querySelector("#bill");
-const peopleEl = document.querySelector("#people");
-const tipButtons = document.querySelectorAll(".option--btn");
-const customEl = document.querySelector("#custom");
-const totalTipEl = document.querySelector("#amount-tip");
-const totalPersonEl = document.querySelector("#amount-person");
-const calculateBtn = document.querySelector("#calculate");
+const bill = document.getElementById("bill");
+const tipBtns = document.querySelectorAll(".option--btn");
+const tipCustom = document.getElementById("custom");
+const people = document.getElementById("people");
+const errorMsg = document.querySelector(".form-control__error");
+const results = document.querySelectorAll(".value");
 const resetBtn = document.querySelector("#reset");
 
-const billAmount = billEl.value.trim();
-const peopleAmount = peopleEl.value.trim();
-let totalPerPerson = billAmount / peopleAmount;
+// initial values
+let billValue = 0.0;
+let tipValue = 0.15;
+let peopleValue = 1;
 
-const calculateTotalPerPerson = () => {
-  totalPerPerson = Math.round(totalPerPerson * 100) / 100;
-  totalPerPerson = totalPerPerson.toFixed(2);
+// validation
+const validateFloat = (s) => {
+  var rgx = /^[0-9]*\.?[0-9]*$/;
+  return s.match(rgx);
+};
 
-  if (billAmount && peopleAmount) {
-    totalPersonEl.innerText = `$${totalPerPerson}`;
-  } else {
-    totalPersonEl.innerText = `$0.00`;
+const validateInt = (s) => {
+  var rgx = /^[0-9]*$/;
+  return s.match(rgx);
+};
+
+const setBillValue = () => {
+  if (bill.value.includes(",")) {
+    bill.value = bill.value.replace(",", ".");
+  }
+
+  if (!validateFloat(bill.value)) {
+    bill.value = bill.value.substring(0, bill.value.length - 1);
+  }
+
+  billValue = parseFloat(bill.value);
+
+  calculateTip();
+};
+
+const handleClick = (event) => {
+  tipBtns.forEach((btn) => {
+    if (event.target.innerHTML == btn.innerHTML) {
+      tipValue = parseFloat(btn.innerHTML) / 100;
+    }
+  });
+
+  //clear custom tip
+  tipCustom.value = "";
+
+  calculateTip();
+};
+
+const setTipCustomValue = () => {
+  if (!validateInt(tipCustom.value)) {
+    tipCustom.value = tipCustom.value.substring(0, tipCustom.value.length - 1);
+  }
+
+  tipValue = parseFloat(tipCustom.value / 100);
+
+  if (tipCustom.value !== "") {
+    calculateTip();
   }
 };
 
-const resetInputs = () => {
-  billEl.value = null;
-  peopleEl.value = null;
-  customEl.value = null;
-  totalTipEl.innerText = "$0.00";
-  totalPersonEl.innerText = "$0.00";
+const setPeopleValue = () => {
+  if (!validateInt(people.value)) {
+    people.value = people.value.substring(0, people.value.length - 1);
+  }
+
+  peopleValue = parseFloat(people.value);
+
+  if (peopleValue <= 0) {
+    errorMsg.classList.add("show-error-msg");
+    setTimeout(function () {
+      errorMsg.classList.remove("show-error-msg");
+    }, 3000);
+  }
+
+  calculateTip();
 };
 
-tipButtons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const value = btn.value;
-    console.log(value);
-  });
-});
+const calculateTip = () => {
+  if (peopleValue >= 1) {
+    let tipAmount = (billValue * tipValue) / peopleValue;
+    let total = (billValue * (tipValue + 1)) / peopleValue;
+    results[0].innerHTML = "$" + tipAmount.toFixed(2);
+    results[1].innerHTML = "$" + total.toFixed(2);
+  }
+};
 
-customEl.addEventListener("input", () => {
-  const custom = customEl.value;
-  console.log(custom);
-});
+const reset = () => {
+  bill.value = "0.0";
+  setBillValue();
 
-calculateBtn.addEventListener("click", () => {
-  calculateTotalPerPerson();
-});
+  tipBtns[2].click();
 
-resetBtn.addEventListener("click", () => {
-  resetInputs();
+  people.value = "1";
+  setPeopleValue();
+};
+
+// event listeners
+bill.addEventListener("input", setBillValue);
+tipBtns.forEach((btn) => {
+  btn.addEventListener("click", handleClick);
 });
+tipCustom.addEventListener("input", setTipCustomValue);
+people.addEventListener("input", setPeopleValue);
+resetBtn.addEventListener("click", reset);
